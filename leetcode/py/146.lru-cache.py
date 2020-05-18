9d9c3608869f7940abf50e7cb7d951dbe13b7d7d
Joe Zheng <joe.zheng@ibm.com>
2020-05-18 14:16:34 -0300

    not working

--- leetcode/py/146.lru-cache.py
+++ leetcode/py/146.lru-cache.py
@@ -1,12 +1,3 @@
-#
-# @lc app=leetcode id=146 lang=python3
-#
-# [146] LRU Cache
-#
-
-# @lc code=start
-
-
 class LinkedNode:
     def __init__(self, key=None, value=None, nextNode=None, prev=None):
         self.key = key
@@ -22,63 +13,66 @@ class LRUCache:
         self.tail = None  # least recent
         self.size = capacity
 
+    def moveToHead(self, currentNode):
+        # put myself in to front
+        currentNode.next = self.head.next
+        self.head.next = currentNode
+
+        # update the prev of previous head
+        self.key2PrevNodeDict[currentNode.next.key] = currentNode
+        self.key2PrevNodeDict[currentNode.key] = self.head
+
     def get(self, key: int) -> int:
         if not key in self.key2PrevNodeDict:
             return -1
 
         prevNode = self.key2PrevNodeDict[key]
-        # print(f'key: {key} prevNode: {prevNode.key} isHead: {self.head}')
 
         if prevNode != self.head:
             # get my node out first
             currentNode = prevNode.next
 
+            if currentNode == self.tail:
+                # print(f"key: {key}, tail: {self.tail.key} 44")
+                self.tail = prevNode
+
             # break chain to skip my node
             prevNode.next = prevNode.next.next
 
-            # now deal with head
-            prevHead = self.head.next
-            self.head.next = currentNode
-            currentNode.next = prevHead
+            self.moveToHead(currentNode)
 
-            self.key2PrevNodeDict[prevHead.key] = currentNode
-            self.key2PrevNodeDict[currentNode.key] = self.head
+        # 2 not workinkg
+        current = self.head.next
+        while current.next:
+            print(f"{key}run {current.key}")
+            current = current.next
 
         return self.head.next.value
 
     def put(self, key: int, value: int) -> None:
         if key in self.key2PrevNodeDict:
+            currentNode = self.key2PrevNodeDict[key].next
+            currentNode.value = value
+            self.get(key)
             return
 
-        # itr = self.head
-        # while itr.next:
-        #     print(itr.value)
-        #     itr = itr.next
-
-        # print(f"adding{key} self.key2PrevNodeDict: {self.key2PrevNodeDict}")
-        # if self.head.next:
-        #     print(f"head: {self.head.next.key}")
-
+        # add new node
         currentNode = LinkedNode(key, value)
 
         # first time
         if self.head.next is None or self.tail is None:
-            print('first time')
+            # print('first time')
             self.tail = currentNode
             self.head.next = currentNode
             self.key2PrevNodeDict[key] = self.head
-        else:
-            # print(f"before swap head: {self.head.next.key}")
-            # put myself in to front
-            currentNode.next = self.head.next
-            self.head.next = currentNode
-            # print(f"after swap head: {self.head.next.key}")
+            return
 
-            # update the prev of previous head
-            self.key2PrevNodeDict[currentNode.next.key] = currentNode
-            self.key2PrevNodeDict[key] = self.head
+        # not first time
+        self.moveToHead(currentNode)
 
         if len(self.key2PrevNodeDict) > self.size:
+            print(f"key: {key}, tail: {self.tail.key} 82")
+
             tailPrev = self.key2PrevNodeDict[self.tail.key]
             # print(
             #     f"key: {key} tailKey: {self.tail.key} Tail's Prev: {tailPrev.value}")
@@ -87,28 +81,18 @@ class LRUCache:
             self.tail = tailPrev
             self.tail.next = None
 
-
-# Your LRUCache object will be instantiated and called as such:
-# obj = LRUCache(capacity)
-# param_1 = obj.get(key)
-# obj.put(key,value)
-# @lc code=end
-
-
-test get
-["LRUCache", "put", "put", "get", "get", "get", "get"]
-[[2], [1, 1], [2, 2], [1], [1], [2], [1]]
-
-
-cache.put(1, 1)
-cache.put(2, 2)
-cache.get(1)  # returns 1
-cache.put(3, 3)  # evicts key 2
-cache.get(2)  # returns - 1 (not found)
-cache.put(4, 4)  # evicts key 1
-cache.get(1)  # returns - 1 (not found)
-cache.get(3)  # returns 3
-cache.get(4)  # returns 4
-
-# [null, null, null, 1, null, 2, null, 1, 3, 4]
-# [null, null, null, 1, null, -1, null, -1, 3, 4]
+# [3]
+# [1, 1]
+# [2, 2]
+# [3, 3]
+# [4, 4] evict 1 -> 4 3 2
+# [4] 432
+# [3],342
+# [2] 234
+# [1], -1
+# [5, 5] evict 4 523
+# [1] - 1,
+# [2]  # should return 2, but -1
+# [3]  # should return 3
+# [4] - 1 didn't evict 4
+# [5]
