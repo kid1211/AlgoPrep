68bd1a06df3293b6d71ef42639ba46bbcfe862df
Joe Zheng <z374201743@gmail.com>
2020-07-21 01:44:50 -0300

    added

--- python/insert-node-in-a-binary-search-tree.py
+++ python/insert-node-in-a-binary-search-tree.py
@@ -13,26 +13,21 @@ class Solution:
     @param: node: insert this node into the binary search tree
     @return: The root of the new binary search tree.
     """
+
     def insertNode(self, root, node):
         # write your code here
-        temp = TreeNode(None)
-        temp.left = root
         if not root:
             return node
 
-        while root:
-            # print(root.val)
-            if node.val <= root.val:
-                if not root.left:
-                    root.left = node
-                    break
-                else:
-                    root = root.left
-            else:
-                if not root.right:
-                    root.right = node
-                    break
-                else:
-                    root = root.right
+        curr = root
 
-        return temp.left
+        while curr != node:
+            if node.val < curr.val:
+                if not curr.left:
+                    curr.left = node
+                curr = curr.left
+            else:
+                if not curr.right:
+                    curr.right = node
+                curr = curr.right
+        return root
